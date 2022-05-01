3b32c0c3c247f30780058c5ad74b5ccf1e3521b7
dantegambarte <da.nt3@hotmail.com>
2022-05-01 13:53:48 -0300

    actualizacion de los registros

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,9 +1,33 @@
-const Contacto = () => {
-    return(
-        <div>
-            <h1>Vista contacto</h1>
-        </div>
-    )
-}
+import "./contacto.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
-export default Contacto
+import contactoIg from "./../img/contacto-ig.jpg";
+import contactoWsp from "./../img/contacto-wsp.jpg";
+import contactoFijo from "./../img/contacto-fijo.jpg";
+
+const iconPhone = (
+  <span>
+    <FontAwesomeIcon icon={faPhone} />
+  </span>
+);
+
+const Contacto = () => {
+  return (
+    <>
+      <h1 className="title-contacto">Forma de contactarnos</h1>
+      <section className="page-content">
+        <hr />
+        <div className="row content-cards">
+          <div className="card-contact">
+            <h2>Contacto vía llamada</h2>
+            <img src={contactoFijo} className="img-contacto"></img>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contacto;
